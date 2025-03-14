87ee769d10a08034ad592aa02db5e15441dcf198
wypchlo <deron.pl.yt@gmail.com>
2025-03-14 11:51:37 +0100

    03/06 done

--- PAW/03/06/main.py
+++ PAW/03/06/main.py
@@ -1,3 +1,6 @@
+import os;
+import shutil;
+
 #1) <-- napisałem to sam
 
 students_file = open("students.txt");
@@ -34,4 +37,16 @@ for line in courses_file:
 #4) ten komentarz nie został nappisany przez sztuczną inteligencje
 
 for student in students:
-    print(student.name, student.surname, f"({student.age} lat):", ", ".join([course.name for course in student.courses]))
+    courses = ", ".join([course.name for course in student.courses]);
+    print(student.name, student.surname, f"({student.age} lat):", courses)
+
+#5)
+
+if os.path.exists("students"): shutil.rmtree("students");
+os.mkdir("students");
+
+for student in students:
+    f = open(f"students/{student.name.lower()}_{student.surname.lower()}.txt", "w+");
+    courses = ",\n - ".join([course.name for course in student.courses]);
+    f.write(f"Kursy: \n - {courses}");
+    f.close();
